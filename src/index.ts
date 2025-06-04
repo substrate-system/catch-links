7d76801a0a5f7f3107a709fe083552133204ea2e
nichoth <nichoth@nichoth.com>
2025-06-04 11:48:52 -0700

    better style

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export function CatchLinks (
     opts:{
         handleAnchor?:boolean|((href:string)=>boolean),
         handleLink?:(href:string)=>boolean,
-    } = { handleAnchor: true },
+    } = {},
 ):()=>void {
     root.addEventListener('click', onClick)
 
@@ -33,7 +33,7 @@ export function CatchLinks (
             }
         }
 
-        // if not anchor, do nothing
+        // if not a link click, do nothing
         if (!anchor) return true
 
         const url = new URL(anchor.getAttribute('href')!, location.origin)
@@ -47,11 +47,13 @@ export function CatchLinks (
             if (!opts.handleLink(urlPath)) return
         }
 
+        const handleAnchor = opts.handleAnchor === undefined ? true : opts.handleAnchor
+
         // else, handle the click
         if (url.href.includes('#')) {
-            if (typeof opts.handleAnchor === 'function') {
+            if (typeof handleAnchor === 'function') {
                 // do we want to handle this?
-                const handle = opts.handleAnchor(url.href)
+                const handle = handleAnchor(url.href)
                 if (handle) {
                     ev.preventDefault()
                     cb(resolve(location.pathname, urlPath || '') +
@@ -59,7 +61,7 @@ export function CatchLinks (
                     return false
                 }
             } else {
-                if (opts.handleAnchor) {
+                if (handleAnchor) {
                     ev.preventDefault()
                     cb(resolve(location.pathname, urlPath || '') +
                         (url.hash || ''))
@@ -83,17 +85,19 @@ CatchLinks.resolve = resolve
 export default CatchLinks
 
 /**
+ * Resolve a local link to a full local path.
+ *
  * @param {string} from
  * @param {string} to
  */
 export function resolve (from:string, to:string):string {
+    const isRelative = (to.charAt(0) !== '/')
+    if (!isRelative) return to
+
     const fromArr = from.split('/')
         .map(path => path.replaceAll('/', ''))
         .filter(Boolean)
     const toArr = to.split('/').map(path => path.replaceAll('/', ''))
-
-    const isRelative = (to.charAt(0) !== '/')
-    if (!isRelative) return to
 
     const str = fromArr.concat(toArr).join('/')
     return str.charAt(0) === '/' ? str : '/' + str
